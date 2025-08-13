b877107b658a3d72b6971e1df8ba92bf537688d7
Dilip Kumar <dilipkumarconnect@gmail.com>
2025-08-13 09:46:03 +0530

    new implements of priority_queue

---
+++ Basics/queue_03/03-Priority_Queue/Priority_queue_Assignment_1/Priority_queue_implementation.py
@@ -0,0 +1,41 @@
+#==============================================================
+# Assignment-16: Priority queue using list
+#==============================================================
+
+'''
+1. Define a class PriorityQueue to implement priority queue data structure using list.
+   Provide __init__() method to create a list object (initially empty).
+
+2. Define a push method in PriorityQueue class to insert new data with given priority.
+
+3. Define a pop method in PriorityQueue class, which returns the highest priority data
+   stored in the priority queue data structure. Raise exception if priority queue is empty.
+
+4. Define is_empty method in PriorityQueue class to check if the priority queue is empty.
+
+5. In class PriorityQueue, define a method size to return the number of elements
+   present in the priority queue.
+   
+'''
+
+
+class Pq:
+    def __init__(self):
+        self.mylist=[]
+        
+    def push(self,value):
+        pass   
+                    
+        
+    def pop(self):
+        self.mylist.pop(0)
+        
+    def is_empty(self):
+        return len(self.mylist)==None
+    
+    def size(self):
+        return len(self.mylist) 
+    
+    
+    
+obj=Pq()           
