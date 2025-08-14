30c7894a40e1701ae8b9bb0cc28dbacd172559cf
Dilip Kumar <dilipkumarconnect@gmail.com>
2025-08-14 08:57:08 +0530

    Add initial implementation of Priority Queue using linked list

---
+++ Basics/queue_03/03-Priority_Queue/Priority_queue_Assignment_2/Priority_queue_implementation.py
@@ -0,0 +1,22 @@
+
+# ========================================================
+# Assignment-17: Priority queue using linked list
+# ========================================================
+
+'''
+1. Define a Node class with instance member variables item, priority and next.
+
+2. Define a class PriorityQueue to implement priority queue data structure using singly
+   linked list. Provide __init__() method to create a start reference variable (initially
+   containing None) and item_count variable (initially 0).
+
+3. Define a push method in PriorityQueue class to insert new data with given priority.
+
+4. Define a pop method in PriorityQueue class, which returns the highest priority data
+   stored in the priority queue data structure. Raise exception if priority queue is empty.
+
+5. Define is_empty method in PriorityQueue class to check if the priority queue is empty.
+
+6. In class PriorityQueue, define a method size to return the number of elements
+   present in the priority queue.
+'''
